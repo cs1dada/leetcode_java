840d68fcbc55ae29035f013cfd840cd63df07edc
cs1dada <cse3dadakiller@gmail.com>
2020-10-14 22:43:09 +0800

    RemoveDuplicatesfromSortedArraySolution

---
+++ top_interview_question/easy/array/RemoveDuplicatesfromSortedArraySolution.java
@@ -0,0 +1,11 @@
+package top_interview_question.easy.array;
+
+/**
+ * RemoveDuplicatesfromSortedArraySolution
+ */
+public class RemoveDuplicatesfromSortedArraySolution {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
