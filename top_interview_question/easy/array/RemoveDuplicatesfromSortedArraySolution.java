95f693df5396538e6246c0886f8afbb3ebec8575
cs1dada <cse3dadakiller@gmail.com>
2020-10-15 17:06:09 +0800

    removeDuplicates

--- top_interview_question/easy/array/RemoveDuplicatesfromSortedArraySolution.java
+++ top_interview_question/easy/array/RemoveDuplicatesfromSortedArraySolution.java
@@ -5,7 +5,29 @@ package top_interview_question.easy.array;
  */
 public class RemoveDuplicatesfromSortedArraySolution {
 
+    public int removeDuplicates(int[] nums) {        
+        //cur代表沒有重複的Array的index
+        int cur = 0;        
+        
+        for (int i=0;i<nums.length;i++){
+            //i會走得比cur快
+            //因為order array的關係只要if成立, 代表發現新的元素
+            if (nums[i]>nums[cur]){
+                nums[++cur] = nums[i];
+            }
+
+        }
+        
+        return cur+1;
+
+    }
+
     public static void main(String[] args) {
+        //int[] numbers = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+        int[] numbers = {1, 2, 3, 3, 4, 5, 6, 6, 6, 7, 8};
+        RemoveDuplicatesfromSortedArraySolution aa = new RemoveDuplicatesfromSortedArraySolution();      
+        int count = aa.removeDuplicates(numbers);  
+        System.out.println(count);
         System.out.println("hello world");
     }
 }
