f6e594aed8489af5585d24ddcf037dcd5c3310fe
cs1dada <cse3dadakiller@gmail.com>
2020-11-29 22:51:04 +0800

    DeleteNodeInALinkedList

--- top_interview_question/easy/linkedList/DeleteNodeInALinkedList.java
+++ top_interview_question/easy/linkedList/DeleteNodeInALinkedList.java
@@ -1,22 +1,42 @@
-package top_interview_question.linkedList;
+package top_interview_question.easy.linkedList;
 
 /**
  * RemoveDuplicatesfromSortedArraySolution
  */
 public class DeleteNodeInALinkedList {
 
-
-
+    /**
+     * //刪除3
+     * 1->2->'3'->4->5->x
+     * 
+     * //先複製下一個點
+     * 1->2->'4'->4->5->x
+     *        |
+     *        ------>5
+     * 
+     * //然後刪除下一個點
+     * 1->2->'4'---->5->x
+     */
     public void deleteNode(ListNode node) {
-        System.out.println(node.val);
+        //因為是single linklist, 所以...
+        //當要刪除一個點, 因為無法讀取前一個node, 所以先複製下一個點, 然後刪除下一個點
+        node.val = node.next.val;
+        node.next = node.next.next;
+        //刪除下一個點
+
     }
 
     public static void main(String[] args) {
 
-        ListNode toDelete = new ListNode(5);
+        ListNode node2 = new ListNode(2);        
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node2.next = node3;
+        node3.next = node4;
 
         DeleteNodeInALinkedList aa = new DeleteNodeInALinkedList();
-        aa.deleteNode(toDelete);
+        aa.deleteNode(node3);
+    
 
         
 
